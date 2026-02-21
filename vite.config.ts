833c228299ba578ecea875ae2e813f0f20e93866
seyyed habibolah Tabasi <Tabasi@DESKTOP-7338EO5>
2026-02-21 17:40:33 +0330

    test commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,75 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+import DefineOptions from "unplugin-vue-define-options/vite";
 
-import type { UserConfigFn } from 'vite'
-
-import { globSync } from 'glob'
-import { join, resolve } from "node:path"
-import { defineConfig } from "vite"
-import vueDocsPlugin from './build/docs-plugin.ts'
-
-// Entry points which we build using vite
-const entryPoints = {
-	...globSync('src/@(components|composables|directives|functions)/*/index.@(js|ts)')
-		.reduce((acc, item) => {
-			const name = item
-				.replace('src/', '')
-				.replace(/\/index\.(j|t)s$/, '/index')
-			acc[name] = join(import.meta.dirname, item)
-			return acc
-		}, {}),
-
-	index: resolve(import.meta.dirname, 'src/index.ts'),
-}
-
-// Customizations for the vite config
-const overrides = defineConfig({
-	plugins: [
-		vueDocsPlugin,
-		l10nPlugin(resolve(import.meta.dirname, 'l10n')),
-	],
-	css: {
-		devSourcemap: true,
-		preprocessorOptions: {
-			scss: {
-				additionalData: '@use "sass:math"; @use "variables" as *; @use "material-icons" as *;',
-				loadPaths: [
-					resolve(import.meta.dirname, 'src/assets'),
-				],
-			},
-		},
-		modules: {
-			// Make sure @nextcloud/vue v9 and @nextcloud/vue v8 have different scopes even for the same component code
-			hashPrefix: '@nextcloud/vue@9',
-			// hashPrefix only works when custom generateScopedName is set
-			// Ref: https://github.com/madyankin/postcss-modules/blob/v6.0.1/src/scoping.js#L39
-			generateScopedName: '_[local]_[hash:base64:5]',
-		},
-	},
-})
-
-// We need a callback config so we can access the vite build mode
-export default defineConfig((env) => {
-	const createConfig = createLibConfig(entryPoints, {
-		// Add our overrides to the config
-		config: overrides,
-		// Only create declarations for source files
-		DTSPluginOptions: {
-			tsconfigPath: 'src/tsconfig.json',
-		},
-		// By default all dependencies are external, but no path imports
-		nodeExternalsOptions: {
-			// Packages with paths imports should be added here to mark them as external as well
-			include: [/^@nextcloud\/.+\//, /^@mdi\/svg\//],
-			// Make sure to not provide uncompiled vue files as dependencies, this will break unit tests
-			exclude: [/\.vue(\?|$)/],
-		},
-		// For backwards compatibility we include the css within the js files
-		inlineCSS: true,
-		// Only standard ESM
-		libraryFormats: ['es'],
-		replace: {
-			PRODUCTION: JSON.stringify(env.mode === 'production'),
-		},
-	})
-
-	return createConfig(env)
-}) as UserConfigFn
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      entryRoot: "./src",
+      outputDir: ["./dist/es/src", "./dist/lib/src"],
+      tsConfigFilePath: "../../tsconfig.json",
+    }),
+    DefineOptions(),
+  ],
+});
